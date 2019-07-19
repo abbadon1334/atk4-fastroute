03b3e5021a75f09c0454c065051bdd3b22e4f356
Francesco Danti <fdanti@gmail.com>
2019-07-19 14:16:49 +0300

    create abstract View for DefaultResponses

---
+++ src/View/AbstractView.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Abbadon1334\ATKFastRoute\View;
+
+use atk4\ui\View;
+use Psr\Http\Message\ServerRequestInterface;
+
+abstract class AbstractView extends View
+{
+    /** @var ServerRequestInterface */
+    protected $request;
+
+    public function __construct(ServerRequestInterface $request, array $defaults = null)
+    {
+        $this->request = $request;
+        parent::__construct($defaults ?? []);
+    }
+}
