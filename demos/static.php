25e3038badd237fb96235ba76e916038dd00b1d2
abbadon1334 <fdanti@gmail.com>
2019-07-19 20:59:26 +0300

    Add Static File serve + Test

---
+++ demos/static.php
@@ -0,0 +1,25 @@
+<?php
+
+include __DIR__.'/bootstrap.php';
+
+use Abbadon1334\ATKFastRoute\Handler\RoutedCallable;
+use Abbadon1334\ATKFastRoute\Handler\RoutedMethod;
+use Abbadon1334\ATKFastRoute\Handler\RoutedServeStatic;
+use Abbadon1334\ATKFastRoute\Handler\RoutedUI;
+use Abbadon1334\ATKFastRoute\Router;
+use atk4\ui\App;
+
+$router = new Router(new App(['always_run' => false]));
+//$router->setBaseDir('/'); // added only for coverage in unit test
+$router->addRoute(
+    '/assets/{path:.+}',
+    ['GET'],
+    new RoutedServeStatic(
+        __DIR__ . '/static_assets',
+        [
+            'css',
+        ]
+    )
+);
+
+$router->run();
---
+++ src/Exception/StaticFileNotAllowedFolder.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Abbadon1334\ATKFastRoute\Exception;
+
+use atk4\core\Exception;
+
+class StaticFileNotAllowedFolder extends \Exception
+{
+
+}
