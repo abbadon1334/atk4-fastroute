e752573520ed0dd8f651660fd6e9ee7584dc3b58
Francesco Danti <fdanti@gmail.com>
2019-07-19 14:18:07 +0300

    refactor MethodNotAllowed

--- src/View/MethodNotAllowed.php
+++ src/View/MethodNotAllowed.php
@@ -1,12 +1,10 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Abbadon1334\ATKFastRoute\View;
 
-use atk4\ui\View;
+use atk4\ui\Exception;
 
-class MethodNotAllowed extends View
+class MethodNotAllowed extends AbstractView
 {
     /**
      * @var array
@@ -14,16 +12,16 @@ class MethodNotAllowed extends View
     protected $_allowed_methods = [];
 
     /**
-     * @throws \atk4\ui\Exception
+     * @throws Exception
      */
     public function init(): void
     {
         parent::init();
 
         $this->add('Header')->set('Method not Allowed');
-        $this->add('View')->set('METHOD : '.getenv('REQUEST_METHOD'));
-        $this->add('View')->set('REQUEST : '.getenv('REQUEST_URI'));
+        $this->add('View')->set('METHOD : ' . $this->request->getMethod());
+        $this->add('View')->set('REQUEST : ' . $this->request->getUri());
 
-        $this->add('View')->set('ALLOWED METHDOS :'.implode(', ', $this->_allowed_methods));
+        $this->add('View')->set('ALLOWED METHDOS :' . implode(', ', $this->_allowed_methods));
     }
 }
